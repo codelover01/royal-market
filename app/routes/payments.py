2c57cbf7da27081124df2a53508dbf560c9bdce5
codelover01 <lovelyexpert1@gmail.com>
2025-01-10 09:20:21 +0300

    Added comments and corrected error in routes/payments.py

--- app/routes/payments.py
+++ app/routes/payments.py
@@ -17,7 +17,9 @@ def create_payment():
         payment_amount(int): amount to be paid
 
     Return:
-
+        - 400 status code for Invalid JSON data
+        and missing required fields.
+        - 200 status code for successful payment.
     """
     data = request.get_json()
     if not data:
@@ -33,15 +35,22 @@ def create_payment():
         return jsonify({"error": "Missing required fields"}), 400
 
     result = PaymentService.create_payment(order_id, payment_method, payment_amount)
-    return jsonify(result)
+    return jsonify({"Payment successful.": result}), 200
 
 
 # Get Payment by ID
 @payment_bp.route('/<int:payment_id>', methods=['GET'])
 def get_payment(payment_id):
+    """
+    Get payment by it's payment ID
+    Args:
+        - payment_id(int): payment ID
+    Returns:
+        - JSON response with 200 status code
+    """
     result = PaymentService.get_payment_by_id(payment_id)
     if isinstance(result, dict):
-        return jsonify(result), 404
+        return jsonify({"Payment details.":result}), 404
     return jsonify({
         "id": result.id,
         "order_id": result.order_id,
@@ -49,26 +58,55 @@ def get_payment(payment_id):
         "payment_method": result.payment_method,
         "payment_amount": result.payment_amount,
         "payment_status": result.payment_status
-    })
+    }), 200
 
 
 # Get All Payments
 @payment_bp.route('/', methods=['GET'])
 def get_all_payments():
-    payments = PaymentService.get_all_payments()
-    result = [{
-        "id": payment.id,
-        "order_id": payment.order_id,
-        "payment_date": payment.payment_date,
-        "payment_method": payment.payment_method,
-        "payment_amount": payment.payment_amount,
-        "payment_status": payment.payment_status
-    } for payment in payments]
-    return jsonify(result)
+    """
+    Gets all the payments.
+    Args:
+        - payments: payments in the database.
+        - result(list): list of all payments.
+    Returns:
+        - A list of payments with status code 200
+        - 404 status code for no payments found
+    """
+    try:
+        payments = PaymentService.get_all_payments()
+        if not payments:
+            return jsonify({"error": "Not payments were found"}), 404
+        result = [{
+            "id": payment.id,
+            "order_id": payment.order_id,
+            "payment_date": payment.payment_date,
+            "payment_method": payment.payment_method,
+            "payment_amount": payment.payment_amount,
+            "payment_status": payment.payment_status
+        } for payment in payments]
+        return jsonify({"Payments found":result}), 200
+    except Exception as e:
+        return jsonify({
+            "error": str(e)
+        }), 500
 
 
 # Refund Payment
 @payment_bp.route('/refund/<int:payment_id>', methods=['POST'])
 def refund_payment(payment_id):
+    """
+    Processes payment refund
+    Args:
+        - payment_id(int): payment ID
+    Returns:
+        - Successful payment refund with status code 200
+    """
     result = PaymentService.refund_payment(payment_id)
-    return jsonify(result)
+    if not result:
+        return jsonify({
+            "error": "Payment ID missing or Invalid"
+        }), 400
+    return jsonify({
+        "Successful payment refund.": result
+        }), 200
