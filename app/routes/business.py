e7dc93576840f9d80561a6483311209c6157b05f
codelover01 <lovelyexpert1@gmail.com>
2025-01-16 13:21:04 +0300

    Added comments

--- app/routes/business.py
+++ app/routes/business.py
@@ -1,3 +1,6 @@
+"""
+A module that deals with business API endpoints
+"""
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from models.business import Business, BusinessException
